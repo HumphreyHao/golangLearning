6d2c93974ee78992b8550011e3f7ac6f8407b743
humphreyHao <haz013@ucsd.edu>
2020-03-26 20:40:57 -0700

    second version with french

--- hello.go
+++ hello.go
@@ -3,19 +3,36 @@ package main
 import "fmt"
 
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenschHelloPrefix = "lalala, "
+const spanish = "Spanish"
+const french = "French"
 
 // Hello ...
 // Solutation : here you tell us what Salutation is
 // Printer : what is this?
 // Greet : describe what this function does
 // CreateMessage : describe what this function does
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
-		name = "world"
+		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenschHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("happy"))
+	fmt.Println(Hello("happy", ""))
 }
