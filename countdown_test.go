12c272ffc221fa18fc01eed255cac7394b3db6fa
HumphreyHao <wangdiao001@gmail.com>
2020-10-11 00:14:53 +0800

    更新

--- countdown_test.go
+++ countdown_test.go
@@ -8,11 +8,11 @@ import (
 
 func TestCountdown(t *testing.T) {
 	buffer := &bytes.Buffer{}
-
+	
 	Countdown(buffer)
 
 	got := buffer.String()
-	want := "4"
+	want := "3"
 
 	if got != want {
 		t.Errorf("got '%s' want '%s'", got, want)
